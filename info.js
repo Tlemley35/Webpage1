90782e49771e74dab32a07983f26a8c7dbd8b4c8
Tlemley35 <35538832+Tlemley35@users.noreply.github.com>
2018-04-18 21:09:41 -0500

    Create info.js

---
+++ info.js
@@ -0,0 +1,6 @@
+var name = "Trey";
+
+document.write("<span id ='footer'>Jacob's Website</span><br />");
+document.write("<p>© 2018 "+name+"<br />");
+document.write("Published on 4/18/18<br />");
+document.write("Retrieved on "+Date()+"</p>");
